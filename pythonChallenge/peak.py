82d8c0c86fdbf739781366adead69b5af4e8d8e0
tomazini.alexandre@gmail.com <tomazini.alexandre@gmail.com>
2016-04-26 16:18:16 -0300

    banner and peak - initial commit

---
+++ pythonChallenge/peak.py
@@ -0,0 +1,11 @@
+# Level 5
+# pronounce it 
+# http://www.pythonchallenge.com/pc/def/peak.html
+# http://www.pythonchallenge.com/pc/def/banner.p
+
+text = open('pythonChallenge/banner.p','r').read()
+print(text)
+
+
+
+# have no idea for now...
